4cf58342a6ed1c571340a49a8b73ea83fdd69ebf
buianh256 <ducanhlccd1@example.com>
2024-02-20 16:06:07 +0700

    done basic login

--- src/App.js
+++ src/App.js
@@ -1,12 +1,11 @@
 
 import './App.css';
+import Login from "./Components/Login";
 
 function App() {
   return (
     <div className="App">
-      <h1 className="text-3xl font-bold underline">
-        Hello world!
-      </h1>
+     <Login/>
     </div>
   );
 }
