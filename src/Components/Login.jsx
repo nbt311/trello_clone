be24cd4225dca8569bf6e9c7335048404a989209
buianh256 <ducanhlccd1@example.com>
2024-02-20 16:34:23 +0700

    done show password

--- src/Components/Login.jsx
+++ src/Components/Login.jsx
@@ -1,5 +1,12 @@
-import React from 'react';
+import React, {useState} from 'react';
+import {FaRegEye, FaRegEyeSlash} from "react-icons/fa";
 const Login = () => {
+    const [passwordVisible, setPasswordVisible] = useState(false);
+
+    const PasswordVisibility = () => {
+        setPasswordVisible(!passwordVisible);
+    };
+
     return (
         <section className="relative bg-gray-100 dark:bg-gray-800 h-screen" >
             <div className="flex flex-col  items-center justify-center h-screen">
@@ -20,12 +27,13 @@ const Login = () => {
                                    className="hover:bg-gray-200 bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600  w-[90%] p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
                                    placeholder="email or username" required=""/>
                         </div>
-                        <div className="pt-3">
+                        <div className="pt-4 relative">
                             <label htmlFor="password"
                                    className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Password</label>
-                            <input type="password" name="password" id="password" placeholder="••••••••"
-                                   className="hover:bg-gray-200 bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600  w-[90%] p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
-                                   required=""/>
+                            <input type={passwordVisible ? "text" : "password"} name="password" id="password" placeholder="••••••••" className="hover:bg-gray-200 bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 w-[90%] p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" required="" />
+                            <button type="button" className="absolute inset-y-0 right-0 px-7 py-14" onClick={PasswordVisibility}>
+                                {passwordVisible ? <FaRegEye /> : <FaRegEyeSlash />}
+                            </button>
                         </div>
                         <div className="pt-3">
                             <button type="submit"
